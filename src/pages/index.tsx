d12dbc4f1101abf997f1aea28ad9b0fb3eaf2754
taoqf <tao_qiufeng@126.com>
2020-12-10 15:48:36 +0800

    server redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,34 @@
 import { NextPage } from 'next';
-import router from 'next/router';
-import { useEffect } from 'react';
+// import router from 'next/router';
+// import { useEffect } from 'react';
 
 interface IProps {
 }
 
-const pg001: NextPage<IProps> = ({ }) => {
-	useEffect(() => {
-		router.replace('pg001');
-	}, []);
+const index: NextPage<IProps> = ({ }) => {
+	// useEffect(() => {
+	// 	router.replace('pg001');	// not a good idea redirecting at client.
+	// }, []);
 	return (
 		<>
 		</>
 	)
 }
 
-export default pg001;
+// to make build success, we must not return redirect at this page at getStaticProps
+index.getInitialProps = async ({ req, res }) => {
+	const url = '/pg001';
+	res.statusCode = 302;
+	const body = `<p>HTTP Redirect: 302. Redirecting to <a href="${url}">${url}</a></p>`;
+	res.setHeader('Content-Length', Buffer.byteLength(body));
+	res.setHeader('Location', url);
+
+	if (req.method === 'HEAD') {
+		res.end();
+	} else {
+		res.end(body);
+	}
+	return {};
+}
+
+export default index;
