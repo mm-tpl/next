b762738547b870aea1c469f4d58e5b71d731340e
taoqf <tao_qiufeng@126.com>
2021-04-29 12:31:10 +0800

    upload file

---
+++ src/pages/api/upload.ts
@@ -0,0 +1,39 @@
+import nextConnect from 'next-connect';
+import { NextApiRequest, NextApiResponse, PageConfig } from 'next';
+import anylogger from 'anylogger';
+import '@mmstudio/an000042';
+import an45 from '@mmstudio/an000045';
+
+const logger = anylogger('pages/api/upload');
+
+export type Result = {
+	ok: true;
+	fileid: string;
+} | {
+	ok: false;
+	message: string;
+};
+
+export type Message = {
+
+}
+
+export type Query = {
+
+}
+
+/**
+ * 上传文件
+ */
+const handler = nextConnect<NextApiRequest, NextApiResponse<Result>>();
+
+handler.put(async (req, res) => {
+	logger.debug('msg body:', req.body);
+	// 解析并保存文件
+	const [file] = await an45(req);
+	res.status(200).json({ ok: true, fileid: file.id });
+});
+
+export const config = {} as PageConfig;
+
+export default handler;
