75f9466b18b98841434dbbbb9c8450065380f4e0
taoqf <tao_qiufeng@126.com>
2022-03-29 13:12:29 +0800

    d

--- docker/push.sh
+++ docker/push.sh
@@ -1,1 +1,8 @@
-docker push 192.168.1.115:6060/gas/gas-app:latest
+docker push 192.168.1.115:6060/01factory/nginx:latest
+docker push 192.168.1.115:6060/01factory/web:latest
+docker push 192.168.1.115:6060/01factory/doccode:latest
+docker push 192.168.1.115:6060/01factory/adminer:latest
+docker push 192.168.1.115:6060/01factory/minio:latest
+docker push 192.168.1.115:6060/01factory/mc:latest
+docker push 192.168.1.115:6060/01factory/postgres:latest
+docker push 192.168.1.115:6060/01factory/kkfileview:latest
