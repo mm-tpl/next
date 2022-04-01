c277f50667d99470acce934682542de39bc2f8aa
taoqf <tao_qiufeng@126.com>
2022-04-01 09:47:12 +0800

    d

---
+++ docker/update.sh
@@ -0,0 +1,8 @@
+docker pull 192.168.1.115:6060/01factory/nginx:latest
+docker pull 192.168.1.115:6060/01factory/web:latest
+docker pull 192.168.1.115:6060/01factory/doccode:latest
+docker pull 192.168.1.115:6060/01factory/adminer:latest
+docker pull 192.168.1.115:6060/01factory/minio:latest
+docker pull 192.168.1.115:6060/01factory/mc:latest
+docker pull 192.168.1.115:6060/01factory/postgres:latest
+docker pull 192.168.1.115:6060/01factory/kkfileview:latest
