4cc1b95a6327d34353672b23119bfd4bea417d86
taoqf <tao_qiufeng@126.com>
2022-04-07 14:20:58 +0800

    change mirror

--- docker/update.sh
+++ docker/update.sh
@@ -1,8 +1,8 @@
-docker pull 192.168.1.115:6060/01factory/nginx:latest
-docker pull 192.168.1.115:6060/01factory/web:latest
-docker pull 192.168.1.115:6060/01factory/doccode:latest
-docker pull 192.168.1.115:6060/01factory/adminer:latest
-docker pull 192.168.1.115:6060/01factory/minio:latest
-docker pull 192.168.1.115:6060/01factory/mc:latest
-docker pull 192.168.1.115:6060/01factory/postgres:latest
-docker pull 192.168.1.115:6060/01factory/kkfileview:latest
+docker pull 114.116.19.243:6060/01factory/nginx:latest
+docker pull 114.116.19.243:6060/01factory/web:latest
+docker pull 114.116.19.243:6060/01factory/doccode:latest
+docker pull 114.116.19.243:6060/01factory/adminer:latest
+docker pull 114.116.19.243:6060/01factory/minio:latest
+docker pull 114.116.19.243:6060/01factory/mc:latest
+docker pull 114.116.19.243:6060/01factory/postgres:latest
+docker pull 114.116.19.243:6060/01factory/kkfileview:latest
