7a0b9664b1b9697cf43e448f3aec86bad5a04483
taoqf <tao_qiufeng@126.com>
2022-04-19 15:36:14 +0800

    simple docker

--- docker/push.bat
+++ docker/push.bat
@@ -1,8 +1,1 @@
-docker push dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/nginx:latest
 docker push dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/web:latest
-docker push dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/doccode:latest
-docker push dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/adminer:latest
-docker push dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/minio:latest
-docker push dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/mc:latest
-docker push dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/postgres:latest
-docker push dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/fileview:latest
--- docker/update.bat
+++ docker/update.bat
@@ -1,8 +1,1 @@
-docker pull dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/nginx:latest
 docker pull dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/web:latest
-docker pull dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/doccode:latest
-docker pull dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/adminer:latest
-docker pull dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/minio:latest
-docker pull dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/mc:latest
-docker pull dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/postgres:latest
-docker pull dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/fileview:latest
