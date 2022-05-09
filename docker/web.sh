7054003be9432a8e35620fb1ffb0fff389193bad
taoqf <tao_qiufeng@126.com>
2022-05-09 14:41:57 +0800

    a

--- docker/web.sh
+++ docker/web.sh
@@ -1,2 +1,2 @@
 ansible-playbook -v ./docker/publish.yml
-curl https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=08d6354d-5048-48f4-8754-943e0e4af2a3 -d '{"msgtype":"text", "text":{ "content": "服务器更新成功:'`date`'"}}'
+curl https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=08d6354d-5048-48f4-8754-943e0e4af2a3 -d '{"msgtype":"text", "text":{ "content": "xxx服务器更新成功:'`date`'"}}'
