66ba4924eb84e3359b4a4a7d76d942a03a5d398f
taoqf <tao_qiufeng@126.com>
2022-05-13 17:06:04 +0800

    pull

--- docker/pull.sh
+++ docker/pull.sh
@@ -1,1 +1,8 @@
-docker-compose -f ./docker-compose.yml pull
+docker pull dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/web:dev
+docker pull dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/db:latest
+docker pull dfactory01-docker.pkg.coding.net/01factory/core/nginx:latest
+docker pull dfactory01-docker.pkg.coding.net/01factory/core/doccode:latest
+docker pull dfactory01-docker.pkg.coding.net/01factory/core/adminer:latest
+docker pull dfactory01-docker.pkg.coding.net/01factory/core/minio:latest
+docker pull dfactory01-docker.pkg.coding.net/01factory/core/mc:latest
+docker pull dfactory01-docker.pkg.coding.net/01factory/core/fileview:latest
