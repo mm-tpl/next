c3234c8e65355fbedc422db8d8c524582b107322
taoqf <tao_qiufeng@126.com>
2022-05-14 13:29:16 +0800

    add bat

---
+++ docker/update.bat
@@ -0,0 +1,8 @@
+docker pull dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/web:dev
+docker pull dfactory01-docker.pkg.coding.net/xxxxxx/xxxxxx/db:latest
+docker pull dfactory01-docker.pkg.coding.net/01factory/core/nginx:latest
+docker pull dfactory01-docker.pkg.coding.net/01factory/core/doccode:latest
+docker pull dfactory01-docker.pkg.coding.net/01factory/core/adminer:latest
+docker pull dfactory01-docker.pkg.coding.net/01factory/core/minio:latest
+docker pull dfactory01-docker.pkg.coding.net/01factory/core/mc:latest
+docker pull dfactory01-docker.pkg.coding.net/01factory/core/fileview:latest
